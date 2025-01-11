5dd4147b6deab1d43241845410578ddc07ad8296
tomoyuki.sato <funkyfunkytomo@gmail.com>
2025-01-11 19:21:23 +0900

    update storage configuration

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import "./../app/app.css";
@@ -8,6 +8,7 @@ import { Amplify } from "aws-amplify";
 import outputs from "@/amplify_outputs.json";
 import "@aws-amplify/ui-react/styles.css";
 import { FileUploader } from '@aws-amplify/ui-react-storage';
+import { uploadData } from 'aws-amplify/storage';
 
 Amplify.configure(outputs);
 
@@ -15,7 +16,25 @@ const client = generateClient<Schema>();
 
 export default function App() {
   const [purchases, setPurchases] = useState<Array<Schema["Purchase"]["type"]>>([]);
+  const [file, setFile] = useState<File | undefined>();
 
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      setFile(files[0]);
+    }
+  };
+  const handleUpload = () => {
+    if (file) { // file が undefined でないことを確認
+        uploadData({
+          path: `picture-submissions/${file.name}`,
+          data: file,
+        })
+        alert(`${file.name}がアップロードされました。`)
+    } else{
+      alert(`ファイルが選択されていません。`)
+    };
+  }
   function listPurcahses() {
     client.models.Purchase.observeQuery().subscribe({
       next: (data) => setPurchases([...data.items]),
@@ -31,7 +50,6 @@ export default function App() {
   //     : window.prompt("Todo content"),
   //   });
   // }
-
   return (
     <main>
       <h1>My todos</h1>
@@ -42,12 +60,13 @@ export default function App() {
         ))}
       </ul>
       <div>
-      <FileUploader
-        acceptedFileTypes={['image/*']}
-        path="public/"
-        maxFileCount={1}
-        isResumable
-      />
+     
+        <input type="file" onChange={handleChange} />
+          <button
+            onClick={handleUpload}
+        >
+          Upload
+        </button>
       </div>
     </main>
   );
